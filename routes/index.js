c091c77350e416faaa2d00dc77da341bff8632a8
DJAwaits <padjcreeper@gmail.com>
2022-03-08 08:41:22 -0800

    second commit slide 59

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ router.get('/accounting', pageController.renderAccounting);
 router.get('/marketing', pageController.renderMarketing);
 router.get('/sales',pageController.renderSales);
 router.get('/hr', pageController.renderHR);
+router.get('/profile', pageController.viewProfile);
 
 router.get('/register', userController.renderRegistrationForm);
 router.post('/register', userController.registerUser);
 router.get('/login', userController.renderLoginForm);
+router.post('/login', userController.loginUser);
 
 module.exports = router;
